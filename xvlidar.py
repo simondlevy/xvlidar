bc9d336e8a4e3864a103898fc8ecf7e3c140641a
Simon D. Levy <simon.d.levy@gmail.com>
2016-01-06 12:54:24 -0500

    Object-oriented

--- xvlidar.py
+++ xvlidar.py
@@ -18,131 +18,141 @@
     GNU General Public License for more details.
 '''
 
+COM_PORT = "/dev/ttyACM0"
 
-import threading, time, serial
+import threading, time, serial, sys
 
-com_port = "/dev/ttyACM0"
-init_level = 0
-index = 0
+class XVLidar(object):
 
-lidarData = [[] for i in range(360)] #A list of 360 elements Angle, Distance , quality
+    def __init__(self, port):
 
-def update( angle, data ):
-    """Updates the view of a sample.
-       Takes the angle (an int, from 0 to 359) and the list of four bytes of data in the order they arrived.
-     """
-    
-    #unpack data using the denomination used during the discussions
-    x = data[0]
-    x1= data[1]
-    x2= data[2]
-    x3= data[3]
-    
-    dist_mm = x | (( x1 & 0x3f) << 8) # distance is coded on 13 bits ? 14 bits ?
-    print(angle, dist_mm)
-    quality = x2 | (x3 << 8) # quality is on 16 bits
-    lidarData[angle] = [dist_mm,quality]
+        self.ser = serial.Serial(port, 115200)
+        self.thread = threading.Thread(target=self._read_lidar, args=())
+        self.thread.daemon = True
+        self.init_level = 0
+        self.index = 0
+        self.lidarData = [[] for i in range(360)] #A list of 360 elements Angle, Distance , quality
+        self.speed_rpm = 0
 
-def checksum(data):
-    """Compute and return the checksum as an int.
-       data -- list of 20 bytes (as ints), in the order they arrived in.
-    """
-    # group the data by word, little-endian
-    data_list = []
-    for t in range(10):
-        data_list.append( data[2*t] + (data[2*t+1]<<8) )
-    
-    # compute the checksum on 32 bits
-    chk32 = 0
-    for d in data_list:
-        chk32 = (chk32 << 1) + d
+    def start(self):
 
-    # return a value wrapped around on 15bits, and truncated to still fit into 15 bits
-    checksum = (chk32 & 0x7FFF) + ( chk32 >> 15 ) # wrap around to fit into 15 bits
-    checksum = checksum & 0x7FFF # truncate to 15 bits
-    return int( checksum )
+        self.thread.start()
+     
+    def _read_lidar(self):
 
+        nb_errors = 0
 
-def compute_speed(data):
-    speed_rpm = float( data[0] | (data[1] << 8) ) / 64.0
-    return speed_rpm
+        while True:
 
-def read_Lidar():
+            try:
 
-    global init_level, index
-    
-    nb_errors = 0
+                time.sleep(0.001) # do not hog the processor power
 
-    while True:
-
-        try:
-            time.sleep(0.001) # do not hog the processor power
-
-            if init_level == 0 :
-                b = ord(ser.read(1))
-                # start byte
-                if b == 0xFA :
-                    init_level = 1
-                else:
-                    init_level = 0
-            elif init_level == 1:
-                # position index
-                b = ord(ser.read(1))
-                if b >= 0xA0 and b <= 0xF9 :
-                    index = b - 0xA0
-                    init_level = 2
-                elif b != 0xFA:
-                    init_level = 0
-            elif init_level == 2 :
-                # speed
-                b_speed = [ ord(b) for b in ser.read(2)]
-                
-                # data
-                b_data0 = [ ord(b) for b in ser.read(4)]
-                b_data1 = [ ord(b) for b in ser.read(4)]
-                b_data2 = [ ord(b) for b in ser.read(4)]
-                b_data3 = [ ord(b) for b in ser.read(4)]
-
-                # for the checksum, we need all the data of the packet...
-                # this could be collected in a more elegent fashion...
-                all_data = [ 0xFA, index+0xA0 ] + b_speed + b_data0 + b_data1 + b_data2 + b_data3
-
-                # checksum
-                b_checksum = [ ord(b) for b in ser.read(2) ]
-                incoming_checksum = int(b_checksum[0]) + (int(b_checksum[1]) << 8)
-
-                # verify that the received checksum is equal to the one computed from the data
-                if checksum(all_data) == incoming_checksum:
-                    speed_rpm = compute_speed(b_speed)
+                if self.init_level == 0 :
+                    b = ord(self.ser.read(1))
+                    # start byte
+                    if b == 0xFA :
+                        self.init_level = 1
+                    else:
+                        self.init_level = 0
+                elif self.init_level == 1:
+                    # position index
+                    b = ord(self.ser.read(1))
+                    if b >= 0xA0 and b <= 0xF9 :
+                        self.index = b - 0xA0
+                        self.init_level = 2
+                    elif b != 0xFA:
+                        self.init_level = 0
+                elif self.init_level == 2 :
+                    # speed
+                    b_speed = [ ord(b) for b in self.ser.read(2)]
                     
-                    update(index * 4 + 0, b_data0)
-                    update(index * 4 + 1, b_data1)
-                    update(index * 4 + 2, b_data2)
-                    update(index * 4 + 3, b_data3)
-                else:
-                    # the checksum does not match, something went wrong...
-                    nb_errors +=1
-                    
-                    # display the samples in an error state
-                    update(index * 4 + 0, [0, 0x80, 0, 0])
-                    update(index * 4 + 1, [0, 0x80, 0, 0])
-                    update(index * 4 + 2, [0, 0x80, 0, 0])
-                    update(index * 4 + 3, [0, 0x80, 0, 0])
-                    
-                init_level = 0 # reset and wait for the next packet
-                
-            else: # default, should never happen...
-                init_level = 0
-        except :
-            exit(0)
+                    # data
+                    b_data0 = [ ord(b) for b in self.ser.read(4)]
+                    b_data1 = [ ord(b) for b in self.ser.read(4)]
+                    b_data2 = [ ord(b) for b in self.ser.read(4)]
+                    b_data3 = [ ord(b) for b in self.ser.read(4)]
 
+                    # for the checksum, we need all the data of the packet...
+                    # this could be collected in a more elegent fashion...
+                    all_data = [ 0xFA, self.index+0xA0 ] + b_speed + b_data0 + b_data1 + b_data2 + b_data3
+
+                    # checksum
+                    b_checksum = [ ord(b) for b in self.ser.read(2) ]
+                    incoming_checksum = int(b_checksum[0]) + (int(b_checksum[1]) << 8)
+
+                    # verify that the received checksum is equal to the one computed from the data
+                    if self._checksum(all_data) == incoming_checksum:
+
+                        self.speed_rpm = self._compute_speed(b_speed)
+                        
+                        self._update(self.index * 4 + 0, b_data0)
+                        self._update(self.index * 4 + 1, b_data1)
+                        self._update(self.index * 4 + 2, b_data2)
+                        self._update(self.index * 4 + 3, b_data3)
+                    else:
+                        # the checksum does not match, something went wrong...
+                        nb_errors +=1
+                        
+                        # display the samples in an error state
+                        self._update(self.index * 4 + 0, [0, 0x80, 0, 0])
+                        self._update(self.index * 4 + 1, [0, 0x80, 0, 0])
+                        self._update(self.index * 4 + 2, [0, 0x80, 0, 0])
+                        self._update(self.index * 4 + 3, [0, 0x80, 0, 0])
+                        
+                    self.init_level = 0 # reset and wait for the next packet
+                    
+                else: # default, should never happen...
+                    self.init_level = 0
+            except:
+                print(sys.exc_info())
+                exit(0)
+
+    def _update(self, angle, data ):
+        """Updates the view of a sample.
+           Takes the angle (an int, from 0 to 359) and the list of four bytes of data in the order they arrived.
+         """
+
+        #unpack data using the denomination used during the discussions
+        x = data[0]
+        x1= data[1]
+        x2= data[2]
+        x3= data[3]
+        
+        dist_mm = x | (( x1 & 0x3f) << 8) # distance is coded on 13 bits ? 14 bits ?
+        print(angle, dist_mm)
+        quality = x2 | (x3 << 8) # quality is on 16 bits
+        self.lidarData[angle] = [dist_mm,quality]
+
+    def _checksum(self, data):
+        """Compute and return the checksum as an int.
+           data -- list of 20 bytes (as ints), in the order they arrived in.
+        """
+        # group the data by word, little-endian
+        data_list = []
+        for t in range(10):
+            data_list.append( data[2*t] + (data[2*t+1]<<8) )
+        
+        # compute the checksum on 32 bits
+        chk32 = 0
+        for d in data_list:
+            chk32 = (chk32 << 1) + d
+
+        # return a value wrapped around on 15bits, and truncated to still fit into 15 bits
+        checksum = (chk32 & 0x7FFF) + ( chk32 >> 15 ) # wrap around to fit into 15 bits
+        checksum = checksum & 0x7FFF # truncate to 15 bits
+        return int( checksum )
+
+
+    def _compute_speed(self, data):
+
+        speed_rpm = float( data[0] | (data[1] << 8) ) / 64.0
+        return speed_rpm
 
 if __name__ == '__main__':
 
-    ser = serial.Serial(com_port, 115200)
-    thread = threading.Thread(target=read_Lidar, args=())
-    thread.daemon = True
-    thread.start()
+    lidar = XVLidar(COM_PORT)
+    lidar.start()
 
     while True:
         try:
